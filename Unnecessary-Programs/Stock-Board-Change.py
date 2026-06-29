1b0d9205435fab37db8093aa6f60f83ec65662e1
benny1232123 <294082426@qq.com>
2026-06-29 23:04:09 +0800

    全流程重构+全云端部署

--- Unnecessary-Programs/Stock-Board-Change.py
+++
@@ -1,9 +0,0 @@
-import akshare as ak
-import pandas as pd 
-import numpy as np
-import time
-
-today = time.strftime("%Y%m%d", time.localtime())
-df=ak.stock_board_change_em()
-df.to_csv(f"stock_data/Stock-Board-Change-{today}.csv", index=False, encoding='utf-8')
-print("Data saved to Stock-Board-Change.csv")
